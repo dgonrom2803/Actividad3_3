1181fdb32128cce96b1a8686013ae74f519d5d0c
dgonrom2803 <dgonrom2803@g.educaand.es>
2022-10-21 12:44:01 +0200

    Update Actividad3_3.java
    Actualización comentarios

--- Actividad3_3.java
+++ Actividad3_3.java
@@ -4,7 +4,7 @@ public class Actividad3_3 {
      * @param args Este es el parametro utilizado en 
      */
         public static void main (String[] args) {
-            //Crear una variable String
+            //Variables
             String s = "Hola";
             String nombre = "Diego";
             String apellido = "González";
@@ -12,14 +12,18 @@ public class Actividad3_3 {
             int a = 2;
             double b = 6.3;
             double resultado2 = 1.5;
-            //Imprimir el valor de la variable
+            //Punto 1
             System.out.println(s);
+            //Punto 2
             System.out.println("Hola " + nombre);
     
-            //Imprimir con formato
+            //Punto 3
             System.out.printf("Hola %s %s, tengo: %d años ",nombre,apellido,edad);
+            //Punto 4
             System.out.printf("\nNOMBRE: %s %s \nEDAD: %d años ",nombre,apellido,edad);
+            //Punto 5
             System.out.printf("\n\"Primero\\Segundo\\Tercero\"");
+            //Punto 6
             System.out.printf("\n%.1f\\%s = %.2f", b, a, resultado2);
     
         
